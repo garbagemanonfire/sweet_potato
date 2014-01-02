386addce8c8b319f47f9d5c8a959eb62f823a23f
garbagemanonfire <nilesvm@gmail.com>
2014-01-01 19:07:45 -0800

    Index action updates

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,6 +4,10 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
+  def index
+    @events = Event.all
+  end
+
   def show
     @event = Event.find(params[:id])
   end
