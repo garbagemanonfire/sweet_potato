936672debe82be9050a29ee793278783b1f5b313
garbagemanonfire <nilesvm@gmail.com>
2014-01-02 12:43:52 -0800

    minor bug in ApplicationController, needed end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
     def after_sign_in_path_for(resource)
        session[:user_return_to] || events_path
     end
@@ -11,5 +12,5 @@ class ApplicationController < ActionController::Base
        unless devise_controller? || request.xhr? || !request.get?
        session[:user_return_to] = request.url
     end
-
+  end
 end
