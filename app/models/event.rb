2fb4197a8dd05f07359c35c86389eb11ca5a4c51
Micah Geisel <micah@botandrose.com>
2014-01-16 21:24:25 -0800

    bring sanity to users -> retreatregs -> events association chain.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,13 +1,10 @@
 class Event < ActiveRecord::Base
-    has_many :retreatregs, foreign_key: "user_id", dependent: :destroy
-    has_many :reverse_retreatregs, foreign_key: "event_id",
-                                   class_name:  "Retreatreg",
-                                   dependent:   :destroy
-    has_many :users, through: :reverse_retreatregs, source: :user
+  has_many :retreatregs, dependent: :destroy
+  has_many :users, through: :retreatregs
 
-    validates :title, presence: true,length: { maximum: 100 }
-    validates :organizer_id, presence: true
-    validates :e_id, presence: true, uniqueness: true
-    VALID_ADDRESS_REGEX = /^[\w\s-]*$/u
-    validates :address_1, presence: true, format: { with: VALID_ADDRESS_REGEX, multiline: true }
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :organizer_id, presence: true
+  validates :e_id, presence: true, uniqueness: true
+  VALID_ADDRESS_REGEX = /^[\w\s-]*$/u
+  validates :address_1, presence: true, format: { with: VALID_ADDRESS_REGEX, multiline: true }
 end
