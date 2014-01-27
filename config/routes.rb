0d40f83b3a72e7919bf57cb4cbc476171dbdbf59
garbagemanonfire <nilesvm@gmail.com>
2014-01-26 20:07:16 -0800

    Added static page for Sendgrid verificaiton

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,9 @@ SpApp::Application.routes.draw do
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  
+  match '/sendgrid', to: 'static_pages#sendgrid', via: 'get'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
